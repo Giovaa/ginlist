13ce58d3c7e1dbe7390d20887f19ea3fe3e68172
Daniele Giovanella <dany94giova@libero.it>
2024-12-28 00:31:31 +0100

    Add files via upload

--- table.js
+++ table.js
@@ -1,8 +1,10 @@
 
-let tableData=[]
-let tableHeaders=[]
-document.addEventListener("DOMContentLoaded", async function(event) { 
-    const csvUrl = 'data/ginlist.csv';
+
+async function render_table (table_name) {
+    let tableData=[]
+    let tableHeaders=[]
+   
+    const csvUrl = `data/${table_name}.csv`;
     const data = await fetch(csvUrl)
         .then(response => response.text())
         .then(str => Papa.parse(str)["data"])
@@ -12,44 +14,20 @@ document.addEventListener("DOMContentLoaded", async function(event) {
     tableHeaders=structuredClone([data[0]])
     data.shift()
     tableData=structuredClone(data)
-    
-    createTable(tableData)
-});
 
-function createTable(data){
-    
-    const thead= document.querySelector("#ginTable thead")
-    thead.innerHTML = '';
-    appendElementToTable("head", thead, tableHeaders)
-
-
-    const tbody= document.querySelector("#ginTable tbody")
-    tbody.innerHTML = '';
-    appendElementToTable("body",tbody,data)
-}
-
-function filterTable()  {
-    const searchInput = document.getElementById("searchInput").value.toLowerCase();
-    const selectedCategory = document.querySelector('input[name="category"]:checked').value;
-
-    // Filter data by search input and category
-    let filteredData = tableData.filter(row => {
-        const nameMatches = row[0].toLowerCase().includes(searchInput);
-        const categoryMatches = selectedCategory === '' || row[1] === selectedCategory;
-        return nameMatches && categoryMatches;
+    DataTable.defaults.layout = {
+        topStart: null,
+        topEnd: null,
+        bottomStart: null,
+        bottomEnd: null
+    };
+    let table = new DataTable('#ginTable',{
+        data: tableData,
+        responsive: true,
+        layout:{
+            top: ['pageLength', 'search', 'buttons'],
+            bottom: ['info', 'paging']
+        },
+        buttons:["csv"]
     });
-    createTable(filteredData)
-}
-
-function appendElementToTable(type,tableElement, data){
-    data.forEach( rowData =>{
-        const tr = document.createElement("tr")
-
-        rowData.forEach(cellData => {
-            const cell = type === "body" ? document.createElement("td") : document.createElement("th")
-            cell.textContent = cellData
-            tr.appendChild(cell)
-        })
-        tableElement.appendChild(tr)
-    })
 }
